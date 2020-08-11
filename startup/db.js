0c95a78bc627516c5fbf96d27a5a3b25745dad97
hariharan <harirock008@gmail.com>
2020-08-11 09:51:32 +0530

    minor changes and big fixed

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const winston = require("winston");
 const mongoose = require("mongoose");
 
 module.exports = function () {
-  const db = process.env.MONGODB_URLL;
+  const db = process.env.MONGODB_URL;
   mongoose.set("useCreateIndex", true);
   mongoose
     .connect(db, {
